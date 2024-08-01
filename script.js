4f70ebcaebf8f5595d826a8254712c814438efd4
Alberto Andreas <albertoandreas99@gmail.com>
2024-08-01 08:42:32 +0300

    Add human choice function with condition to only choose between rock, paper and scissors

--- script.js
+++ script.js
@@ -4,4 +4,27 @@ function getComputerChoice() {
   console.log(randomChoice);
 }
 
-getComputerChoice();
+function getHumanChoice() { 
+  let humanChoice = prompt("Rock, Paper or Scissors? The coice is yours:");
+
+  if (humanChoice === "Rock") {
+    return console.log("You have chosen Rock");
+  } else if (humanChoice === "Paper") {
+    return console.log("You have chosen Paper");
+  } else if (humanChoice === "Scissors") {
+    return console.log("You have chosen Scissors");
+  } else if (humanChoice === "rock") {
+    return console.log("You have chosen Rock");
+  } else if (humanChoice === "paper") {
+    return console.log("You have chosen Paper");
+  } else if (humanChoice === "scissors") {
+    return console.log("You have chosen Scissors");
+  } else {
+    return console.log ("You can only choose between Rock, Paper or Scissors!")
+  }
+
+  
+  
+}
+
+getHumanChoice();
