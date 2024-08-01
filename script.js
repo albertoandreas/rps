df0315c89411d814d87992afe048b0415c6c08e4
Alberto Andreas <albertoandreas99@gmail.com>
2024-08-01 10:11:28 +0300

    Modify human choosing condition to case insensitive and clean up code

--- script.js
+++ script.js
@@ -3,27 +3,28 @@ let computerScore = 0;
 
 
 function getComputerChoice() {
-  const compChoice = ["Rock", "Paper", "Scissors"];
-  const randomChoice = compChoice[(Math.floor(Math.random() * (compChoice.length)))];
-  console.log(randomChoice);
+  const computerChoice = ["Rock", "Paper", "Scissors"];
+  const randomChoice = computerChoice[(Math.floor(Math.random() * (computerChoice.length)))];
+  console.log("The Computer has chosen " + randomChoice);
 }
 
 function getHumanChoice() { 
   let humanChoice = prompt("Rock, Paper or Scissors? The coice is yours:");
-
-  if (humanChoice === "Rock") {
-    return console.log("You have chosen Rock");
-  } else if (humanChoice === "Paper") {
-    return console.log("You have chosen Paper");
-  } else if (humanChoice === "Scissors") {
-    return console.log("You have chosen Scissors");
-  } else if (humanChoice === "rock") {
-    return console.log("You have chosen Rock");
-  } else if (humanChoice === "paper") {
-    return console.log("You have chosen Paper");
-  } else if (humanChoice === "scissors") {
-    return console.log("You have chosen Scissors");
-  } else {
-    return console.log ("You can only choose between Rock, Paper or Scissors!")
+  switch(humanChoice = humanChoice.toUpperCase()) {
+    case "ROCK" : console.log("You chose Rock");
+    break
+    case "PAPER" : console.log("You chose Paper");
+    break
+    case "SCISSORS" : console.log("You chose Scissors");
+    break
+    default : console.log("You can only choose between Rock, Paper and Scissors!");
   }
 }
+
+function playRound(humanChoice, computerChoice) {
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
